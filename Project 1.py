cf566b7e5d5d06f25c7ba166c3d52550ce872d08
harshith044 <80192037+harshith044@users.noreply.github.com>
2021-06-25 20:13:53 +0530

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,39 @@
+##Project1:Basic school administration tool
+
+import csv
+
+def write_into_csv(info_list):
+    with open('student_info.csv','w+',newline=' ')as csv_file:
+        writer=csv.writer(csv_file)
+        if csv_file.tell()==0:
+            writer.writerow("Name","Age","Contact","EmailId")
+        writer.writerow(info_list)
+condition =True
+
+if __name__=='__main__':
+    condition=True
+    student_num=1
+    
+    while(condition):
+        student_info=input("Enter student information for student #{} in the following format (Name Age Contact Email_id: ")
+        print("Entered information: "+student_info)
+
+    #split
+    student_info_list=student_info.split(' ')
+    print("Enterd split ip information list: "+str(student_info_list));
+
+    print("\nThe Entered information is -\nName:{}\nAge: {}\nContact Number: {}\nE-Mail ID: {}"
+          .format( student_info_list[0],  student_info_list[1] , student_info_list[2]))
+
+    choice_check=input("Is the entered information correct (yes/no): ")
+
+    if choice_check=="yes":
+            write_into_csv(student_info_list)
+            condition_check=input("Enter (yes/no) If you want to enter information of other student: ");
+            if condition_check=="yes":
+                 condition=True
+                 student_num=student_num+1
+            elif condition_check=="no":
+                 condition=False
+        elif choice_check=="no":
+            print("\nPlease re-enter values!")
